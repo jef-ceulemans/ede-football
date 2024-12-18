59925c4d6b1feed756fb8790ef796b181dc13bd8
jef-ceulemans <115020779+jef-ceulemans@users.noreply.github.com>
2024-12-18 16:15:20 +0100

    dieje maven e nietfdsqdfd

--- match-service/src/main/java/fact/it/matchservice/model/Match.java
+++ match-service/src/main/java/fact/it/matchservice/model/Match.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
