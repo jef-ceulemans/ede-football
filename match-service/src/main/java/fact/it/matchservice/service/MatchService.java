b7a1c204d6a5ab0e7c2cd5cc4d77ad5831def253
jef-ceulemans <115020779+jef-ceulemans@users.noreply.github.com>
2024-12-19 14:15:02 +0100

    test data voor match nog s

--- match-service/src/main/java/fact/it/matchservice/service/MatchService.java
+++ match-service/src/main/java/fact/it/matchservice/service/MatchService.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import jakarta.annotation.PostConstruct;
 
 import jakarta.transaction.Transactional;
 import java.util.List;
