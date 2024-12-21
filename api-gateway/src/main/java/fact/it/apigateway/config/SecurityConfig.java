c3e7adc99f939b327e2d0e92f203dcd5d5bf99e6
jef-ceulemans <115020779+jef-ceulemans@users.noreply.github.com>
2024-12-21 21:51:43 +0100

    /api prefixt

--- api-gateway/src/main/java/fact/it/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/fact/it/apigateway/config/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig {
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
         serverHttpSecurity
                 .authorizeExchange(exchange ->
-                        exchange.pathMatchers(HttpMethod.GET,"/match/all")
+                        exchange.pathMatchers(HttpMethod.GET,"/api/match/all")
                                 .permitAll()
                                 .anyExchange()
                                 .authenticated()
